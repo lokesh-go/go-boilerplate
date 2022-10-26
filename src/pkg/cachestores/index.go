60ab367a6d4e50be96269372d83ea17f37ca7a98
lokesh.chandra <lokesh.chandra@bookmyshow.com>
2022-10-27 00:03:54 +0530

    adds pkg directory

---
+++ src/pkg/cachestores/index.go
@@ -0,0 +1,3 @@
+package cachestores
+
+// Here we can defines differents cache mechanism like: appcache, rediscache etc. with separate folder
---
+++ src/pkg/datastores/index.go
@@ -0,0 +1,3 @@
+package datastores
+
+// Here we can define differents data store function with separate folder like: mongodb etc.
